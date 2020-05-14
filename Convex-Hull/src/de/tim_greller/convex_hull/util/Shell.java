e75e171718ab51e7e6a4ca168925b4596b702341
Tim Greller <timlg07@gmail.com>
2020-05-14 23:02:45 +0200

    Created Shell class.

---
+++ Convex-Hull/src/de/tim_greller/convex_hull/util/Shell.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package de.tim_greller.convex_hull.util;
+
+/**
+ * 
+ */
+public final class Shell {
+
+    /** Private constructor to prevent instantiation. */
+    private Shell() { }
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
