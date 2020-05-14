920475769050e1fe15e567432a21736249cac8ea
Tim Greller <timlg07@gmail.com>
2020-05-14 23:30:12 +0200

    Basic implementation of Point.

--- Convex-Hull/src/de/tim_greller/convex_hull/model/Point.java
+++ Convex-Hull/src/de/tim_greller/convex_hull/model/Point.java
@@ -4,8 +4,39 @@
 package de.tim_greller.convex_hull.model;
 
 /**
- *
+ * Represents a point in a two-dimensional integer coordinate system.
  */
 public class Point {
 
+    /** The x and y coordinates of the point. */
+    private int x, y;
+    
+    /**
+     * Creates a new Point with the given position.
+     * 
+     * @param x The first coordinate.
+     * @param y The second coordinate.
+     */
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    /**
+     * Returns the x coordinate.
+     * 
+     * @return The first coordinate.
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * Returns the y coordinate.
+     * 
+     * @return The second coordinate.
+     */
+    public int getY() {
+        return y;
+    }
 }
