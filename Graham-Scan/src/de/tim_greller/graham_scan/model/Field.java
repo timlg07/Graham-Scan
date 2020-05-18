469fdc65c3d48e82d44a9704be13a9bea16b64cb
Tim Greller <timlg07@gmail.com>
2020-05-18 16:35:38 +0200

    Created field class.

---
+++ Graham-Scan/src/de/tim_greller/graham_scan/model/Field.java
@@ -0,0 +1,16 @@
+package de.tim_greller.graham_scan.model;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *
+ */
+public class Field {
+    
+    private List<Point> points = new LinkedList<Point>();
+    
+    public Field() {
+        
+    }
+}
