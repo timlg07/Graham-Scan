4964bbcecfb6895deed535346d82329a9cf09a53
Tim Greller <timlg07@gmail.com>
2020-05-22 00:46:06 +0200

    Implemented the basics of AngleDistComparator.

---
+++ Graham-Scan/src/de/tim_greller/graham_scan/model/AngleDistComparator.java
@@ -0,0 +1,54 @@
+package de.tim_greller.graham_scan.model;
+
+import java.util.Comparator;
+
+/**
+ * Compares two points based on their position relative to the point p0.
+ */
+public class AngleDistComparator implements Comparator<Point> {
+    
+    private Point p0;
+    
+    /**
+     * A Comparator for the given {@link Point}.
+     * @param p0 The point other points should get compared with.
+     */
+    public AngleDistComparator(Point p0) {
+        this.p0 = p0;
+    }
+    
+    /**
+     * Compares two points {@code p} and {@code q} according to the angle of
+     * their vectors to p0. If the angle is equal, the vectors get compared by
+     * size.
+     * 
+     * @param p The first {@link Point}.
+     * @param q The second {@link Point}.
+     * @return A value greater than zero if the vector from p0 to p comes before
+     *         the vector from p0 to q if you sort them clockwise around p0 or 
+     *         if all points are on one line and the vector to q is shorter than
+     *         the one to p. Zero if both points are equal. A negative value 
+     *         else.
+     */
+    public int compare(Point p, Point q) {
+        int result = p0.leftOf(p, q);
+        // All points on one line => Same angle -> Compare distance.
+        if (result == 0) {
+            result = (int)(calculateDistance(p0, p) - calculateDistance(p0, q));
+        }
+        return result;
+    }
+    
+    /**
+     * Calculates the distance between two {@link Point}s.
+     * 
+     * @param a The first {@link Point}.
+     * @param b The second {@link Point}.
+     * @return The magnitude of the vector from {@code a} to {@code b}.
+     */
+    public double calculateDistance(Point a, Point b) {
+        int vectorX = b.getX() - a.getX();
+        int vectorY = b.getY() - a.getY();
+        return Math.sqrt(vectorX * vectorX + vectorY * vectorY);
+    }
+}
