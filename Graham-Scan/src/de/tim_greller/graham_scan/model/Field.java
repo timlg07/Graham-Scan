0c01318bf94d40f4cb175069cf166c4fdb38126d
Tim Greller <timlg07@gmail.com>
2020-05-22 16:58:52 +0200

    Implemented graham scan caller.
    Optimized to avoid multiple execution of the same algorithms.

--- Graham-Scan/src/de/tim_greller/graham_scan/model/Field.java
+++ Graham-Scan/src/de/tim_greller/graham_scan/model/Field.java
@@ -4,13 +4,27 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+
 /**
  *
  */
 public class Field {
     
+    /** The current set of points */
     private List<Point> points = new ArrayList<Point>();
+    
+    /** The last calculated convex hull */
+    private List<Point> convex = new ArrayList<Point>();
+    
+    /** Indicates if {@code points} has been modified since it was sorted. */
     private boolean isSorted = true;
+    
+    /** 
+     * Indicates if {@code points} has been modified since the last convex 
+     * hull was calculated.
+     */
+    private boolean isConvexUpToDate = true;
+    
     
     /**
      * Appends a {@link Point} to the field if the point is not already in it.
@@ -20,6 +34,7 @@ public class Field {
      */
     public boolean add(Point p) {
         isSorted = false;
+        isConvexUpToDate = false;
         return !points.contains(p) && points.add(p);
     }
     
@@ -29,6 +44,7 @@ public class Field {
      * @return {@code true} if {@code p} was removed successful.
      */
     public boolean remove(Point p) {
+        isConvexUpToDate = false;
         return points.remove(p);
     }
     
@@ -47,8 +63,35 @@ public class Field {
         }
     }
     
-    // This is correct in some cases, good enough.
+    /**
+     * Calculates the convex hull of all {@link Point}s stored in this 
+     * {@link Field} using the graham scan algorithm. <br> If the set of points 
+     * stored in the field was not modified since the last execution of this
+     * method, a new list containing the same points will get returned without 
+     * running the algorithm again.
+     * 
+     * @return The {@link Point}s forming the convex hull, starting with the
+     *         {@link Point} furthest to the left and bottom and continuing 
+     *         counter clockwise.
+     */
     public List<Point> convexHull() {
-        return points;
+        if (!isConvexUpToDate) {
+            convex = grahamScan();
+            isConvexUpToDate = true;
+        }
+        // Return a new list so the stored one cannot get modified.
+        // Using the Constructor is sufficient because each Point is immutable.
+        return new ArrayList<Point>(convex);
+    }
+    
+    /**
+     * Implementation of the graham scan algorithm.
+     * 
+     * @return The {@link Point}s forming the convex hull, starting with the
+     *         {@link Point} furthest to the left and bottom and continuing 
+     *         counter clockwise.
+     */
+    private List<Point> grahamScan() {
+        throw new UnsupportedOperationException("Not implemented yet");
     }
 }
